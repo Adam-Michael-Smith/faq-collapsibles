14e1c9410ee29ad924525d7df1f39adf11e75d51
Adam <92633320+Adam-Michael-Smith@users.noreply.github.com>
2021-11-29 22:03:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+
+// <-------------- MOBILE MENU -------------->
+
+const mobileNav = document.getElementsByClassName("mobile-nav");
+const popUpNav = document.getElementsByClassName("nav");
+
+for (let i = 0; i < mobileNav.length; i++) {
+
+  mobileNav[i].addEventListener("click", openMobileMenu);
+
+  function openMobileMenu(){
+  if(popUpNav[i].style.display === "block"){
+    popUpNav[i].style.display = "none";
+  } else{
+    popUpNav[i].style.display = "block";
+  }
+}
+}
+
 // <-------------- FAQ DROPDOWN SECTION -------------->
 
 const question = document.getElementsByClassName('question');
@@ -21,22 +40,4 @@ for (let i = 0; i < question.length; i++) {
       questionOf[i].style.background = "#141414";
     }
   }
-}
-
-// <-------------- MOBILE MENU -------------->
-
-const mobileNav = document.getElementsByClassName("mobile-nav");
-const popUpNav = document.getElementsByClassName("nav");
-
-for (let i = 0; i < mobileNav.length; i++) {
-
-  mobileNav[i].addEventListener("click", openMobileMenu);
-
-  function openMobileMenu(){
-  if(popUpNav[i].style.display === "block"){
-    popUpNav[i].style.display = "none";
-  } else{
-    popUpNav[i].style.display = "block";
-  }
-}
 }
